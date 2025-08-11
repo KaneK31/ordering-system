e7737100dc072d5579c1ede8259660787f51371c
Kane <Kanekerr6@gmail.com>
2025-08-11 10:26:56 +0100

    updated settings panel

--- Platform/admin.py
+++ Platform/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order, OrderItem
 
-# # Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ("product", "quantity", "price")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "created_at", "is_paid", "total_price")
+    search_fields = ("id", "invoice_id", "user__username", "user__email")
+    list_filter = ("is_paid", "created_at")
+    inlines = [OrderItemInline]
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'product_price', 'product_stock', 'created_at')
-    search_fields = ('product_name',)
-    list_filter = ('created_at',)
+    list_display = ("product_name", "product_price", "product_stock")
+
+
+# Optional if you want to see order items as a separate model too:
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "product", "quantity", "price")
